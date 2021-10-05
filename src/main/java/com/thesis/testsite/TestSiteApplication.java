b3740961e19d1a7ddb418d7712b5f5c2010658d4
Tarr Márton <tarr.marton1@gmail.com>
2021-10-05 20:58:03 +0200

    initial commit

---
+++ src/main/java/com/thesis/testsite/TestSiteApplication.java
@@ -0,0 +1,13 @@
+package com.thesis.testsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSiteApplication.class, args);
+    }
+
+}
