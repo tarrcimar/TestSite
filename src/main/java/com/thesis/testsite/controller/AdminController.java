a2a22767c4f188e580903018a672a4abf31d24bd
Tarr Márton <tarr.marton1@gmail.com>
2021-10-10 21:03:55 +0200

    Added xml import

--- src/main/java/com/thesis/testsite/controller/AdminController.java
+++ src/main/java/com/thesis/testsite/controller/AdminController.java
@@ -2,12 +2,24 @@ package com.thesis.testsite.controller;
 
 import com.thesis.testsite.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class AdminController {
+
 
     private UserService userService;
 
@@ -21,6 +33,28 @@ public class AdminController {
         System.out.println("Id: " + messageId);
         userService.deleteMessage(messageId);
         return "redirect:/";
+    }
+
+    @RequestMapping("/adminPanel")
+    public String adminPanel(){
+        return "adminPanel";
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public String upload(@RequestParam("fileToUpload") MultipartFile document) throws IOException, ParserConfigurationException, SAXException {
+        File tempFile = File.createTempFile("valami", ".xml");
+        tempFile.deleteOnExit();
+        document.transferTo(tempFile);
+        System.out.println("transfered");
+        System.out.println(document);
+        System.out.println(tempFile);
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document document1 = db.parse(tempFile);
+        System.out.println(document1.toString());
+
+        return "redirect:/adminPanel";
     }
 
 }
